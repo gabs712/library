37fe92a21b13933ad387459bd5a3c4e53143c8b1
gabs712 <gabs712.g@gmail.com>
2024-04-15 18:26:14 -0300

    Add library logic

--- script.js
+++ script.js
@@ -1,14 +1,93 @@
-const createButton = document.querySelector('.header-create')
 const modal = document.querySelector('.modal')
+const openModalButton = document.querySelector('.header-create')
+const createBookButton = document.querySelector('.modal-create-button')
+const cancelBookButton = document.querySelector('.modal-cancel-button')
+const form = document.querySelector('.modal-form')
+const booksGrid = document.querySelector('.books-grid')
 
-createButton.addEventListener('click', () => {
+const myLibrary = []
+
+displayBooks()
+
+function Book(title, author, pages, isRead) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.isRead = isRead
+}
+
+Book.prototype.toggleRead = function() {
+  this.isRead = this.isRead ? false : true
+}
+
+function addBookToLibrary() {
+  const title = form.title.value
+  const author = form.author.value
+  const pages = form.pages.value
+  const isRead = form.isRead.checked
+
+  myLibrary.push(new Book(title, author, pages, isRead))
+}
+
+function displayBooks() {
+  booksGrid.innerHTML = ''
+
+  myLibrary.forEach((book, i) => {
+    const newBookCard = document.createElement('div')
+    newBookCard.classList.add('book-card')
+
+    let readClass
+    let readContent
+    if (book.isRead) {
+      readClass = 'book-read-button'
+      readContent = 'Read'
+    } else {
+      readClass = 'book-notRead-button'
+      readContent = 'Not Read'
+    }
+  
+    newBookCard.innerHTML = `
+      <div class="book-card-title">"${book.title}"</div>
+      <div class="book-card-author">${book.author}</div>
+      <div class="book-card-pages">${book.pages} pages</div>
+      <div class="book-card-buttons">
+        <button data-read class="${readClass}">${readContent}</button>
+        <button data-remove class="book-remove-button" >Remove</button>
+      </div>
+    `
+
+    newBookCard.querySelector('[data-read]').addEventListener('click', () => {
+      book.toggleRead()
+      displayBooks()
+    })
+
+    newBookCard.querySelector('[data-remove]').addEventListener('click', () => {
+      myLibrary.splice(i, 1)
+      displayBooks()
+    })
+
+    booksGrid.insertAdjacentElement('afterbegin', newBookCard)
+  })
+} 
+
+form.addEventListener('submit', () => {
+  addBookToLibrary()
+  displayBooks()
+  form.reset()
+})
+
+openModalButton.addEventListener('click', () => {
   modal.showModal()
 })
 
 modal.addEventListener('click', (e) => {
-  if(e.target === modal) {
+  if (e.target === modal) {
     modal.close()
+    form.reset()
   }
 })
 
-console.dir(document.querySelector('form'))
+cancelBookButton.addEventListener('click', () => {
+  modal.close()
+  form.reset()
+})
